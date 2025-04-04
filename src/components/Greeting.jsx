8eb15061d64e19a35b9a6443e1a25cb7ac33f2ee
Luanashinsato <lshinsato@usf.edu>
2025-04-04 18:48:50 -0400

    Task 1: Creating a New Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,15 @@
+// Task 1: Creating a New Component
+import React from 'react'; // Importing React library
+
+// Defining a functional React component named Greeting
+function Greeting() {
+    // The component returns a JSX block containing a heading and paragraph
+  return (
+    <div>
+        <h2>Hello from the Greeting Component!</h2>
+        <p>This is your first custom component in React.</p>
+    </div>
+  );
+}
+// Export the Greeting component so it can be used in other files
+export default Greeting;
